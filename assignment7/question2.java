cda0ba98df745302e424ee76d6bee16e4bfb0d53
mysecndgithub <60555134+mysecndgithub@users.noreply.github.com>
2020-02-01 22:01:36 +0100

    Add files via upload

---
+++ assignment7/question2.java
@@ -0,0 +1,18 @@
+package assignments.assignment_5;
+
+public class question2 {
+
+	public static void main(String[] args) {
+		// Author: CA, Date: 2020-01-10
+		int num1 = 6, num2 = 4;
+		if (num1 == num2) {
+			System.out.println(num1 + " and " + num2 + " are equal");
+		} else if (num2 < num1) {
+			System.out.println(num1 + " is greater than " + num2);
+		} else if (num2 > num1) {
+			System.out.println(num2 + " is greater than " + num1);
+		}
+
+	}
+
+}
